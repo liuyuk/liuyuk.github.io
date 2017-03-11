7df644024891104fb66c616f5198b18802421317
liuyuk <kimliu9411@gmail.com>
2017-03-11 18:38:25 -0500

    edited front page
    dynamic portfolio button not working

--- site_script.js
+++ site_script.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
+    $(".nav_bar").delay(200).fadeIn();
+    $(".main_body").delay(500).fadeIn();
+    
     var x, y = 1;
     $("li.about").click(function(){
         if (x) clearTimeout(x);
         x = setTimeout(function() {
             var current_length = $(".about_container").width();
             if (current_length < 3) {
-                $(".about_container").fadeIn();
-                $(".about_container").fadeOut();
-                $(".about_container").fadeIn();
+                $(".main_body").fadeOut();
+                $(".about_container").delay(300).fadeIn();
                 $(".about_container").animate({
                     top: '10%',
                     height: '75%'
@@ -16,14 +18,14 @@ $(document).ready(function(){
                     left: '7%',
                     width: '80%'
                 }, 650);
-                $("p.about_body").delay(2500).fadeIn();
-                $("p.about_aside").delay(2500).fadeIn();
-                $("img.about_photo").delay(2500).fadeIn();
+                $("p.about_body").delay(2000).fadeIn();
+                $("p.about_aside").delay(2000).fadeIn();
+                $("img.about_photo").delay(2000).fadeIn();
             } else {
                 $("img.about_photo").fadeOut();
                 $("p.about_aside").fadeOut();
                 $("p.about_body").fadeOut();
-                $(".about_container").delay(500).animate({
+                $(".about_container").delay(300).animate({
                     left: '47%',
                     width: '2px'
                 }, 650);
@@ -32,10 +34,38 @@ $(document).ready(function(){
                     height: '2px'
                 }, 650);
                 $(".about_container").fadeOut();
-                $("a.about:visited").css("color: gray");
+                $(".main_body").delay(1600).fadeIn();
             }
             y++;
             console.log(y);
-        }, 500);
+        }, 300);
     });
+    
+//    $("h2.body_button").hover(function(){
+//        $(this).animate({
+//            borderTopColor: 'black'
+//        }, 200);
+//        $(this).animate({
+//            borderRightColor: 'black'
+//        }, 200);
+//        $(this).animate({
+//            borderBottomColor: 'black'
+//        }, 200);
+//        $(this).animate({
+//            borderLeftColor: 'black',
+//            color: 'black'
+//        }, 200, function() {
+//            $(this).css("border", "1px solid black");
+//            $(this).css("color", "black");
+//        });
+//
+//    }, function(){
+//        $(this).animate({
+//            borderColor: 'lightgray',
+//            color: 'lightgray'
+//        }, 200, function() {
+//            $(this).css("border", "1px solid lightgray");
+//            $(this).css("color", "lightgray");
+//        });
+//    });
 });
